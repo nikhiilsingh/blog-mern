e3a98c16a13fdf71229b4e866a4e6f742c959cd4
Nikhil Singh <nikhilrajput4265@gmail.com>
2025-09-01 14:50:20 +0530

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import {BrowserRouter, Routes , Route} from 'react-router-dom'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Signin } from './pages/Signin'
+import { Signup } from './pages/Signup'
+import { Dashboard } from './pages/Dashboard'
+
+
+
 
 const App = () => {
   return (
-    <div className='text-3xl text-red-600'>Nikhil Teest</div>
+    <BrowserRouter>
+
+    <Routes>
+
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/sign-in' element={<Signin/>}></Route>
+      <Route path='/sign-up' element={<Signup/>}></Route>
+      <Route path='/dashboard' element={<Dashboard/>}></Route>
+
+    </Routes>
+
+    </BrowserRouter>
   )
 }
 
